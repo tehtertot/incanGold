627d9b6184cf9871f6cc36e31b4f5f8b0e1889ef
mathew <mathewjcohen@gmail.com>
2017-07-07 15:22:03 -0700

    added all the things - game logic, new round, end game - kinda, alrets for new rounds... etc.

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ class Player {          ////////////PLAYER/////////////
     this.id = id;
     this.username = name;
     this.points = 0;
-    this.choice = 1;
+    this.currentTreasure = 0;
   }
   goBack(currentTreasure) {
     this.points += currentTreasure;
@@ -90,18 +90,105 @@ class Game {
 }
 
 var players = [];
+var round = 1;
 var deck = new Deck();
 var roundTreasure = 0;
-var playersInRound = [];
+var playersStillPlaying = {};
+var playersAboutToLeave = {};
+var playersAlreadyGone = {};
+var playerResponses = 0;
 
 var io = require('socket.io').listen(server);
 io.sockets.on('connection', function (socket) {
     function checkStatus(){
-        
+        if(playerResponses + Object.keys(playersAlreadyGone).length == players.length){
+            // count how many players left
+            let countLeaving = Object.keys(playersAboutToLeave).length;
+            for(let p in playersAboutToLeave){
+                for(let idx in players){
+                    if (players[idx].id == p){
+                        players[idx].points += Math.floor(roundTreasure / countLeaving) + players[idx].currentTreasure;
+                        players[idx].currentTreasure = 0;
+                    }
+                }
+                playersAlreadyGone[p] = true;
+                roundTreasure = roundTreasure % countLeaving;
+                playersAboutToLeave = {};
+            }
+
+            
+            //if the card drawn is a duplicate of a hazard card already in play, all players still playing lose all their currentTreasure and the round is over
+
+            //one of the duplicate hazard cards is removed from the deck
+            //otherwise :
+
+            //for everyone staying
+            if (Object.keys(playersStillPlaying).length > 0){
+                var card = deck.drawCard();
+                io.emit("showCard", {card: card, allPlayers: players});
+                if(card.val == 0){
+                    for(let i=0; i<deck.inPlay.length-1; i++){
+                        if(deck.inPlay[i].name == card.name){
+                            deck.inPlay.pop();
+                            for(let p in playersStillPlaying){
+                                for(let idx in players){
+                                    if (players[idx].id == p){
+                                        players[idx].currentTreasure = 0;
+                                    }
+                                }
+                            }
+                            playerResponses = 0;
+                            endRound();
+                            return;
+                        }
+                    }
+                }else{
+                    for(let p in playersStillPlaying){
+                        for(let idx in players){
+                            if (players[idx].id == p){
+                                players[idx].currentTreasure += Math.floor(card.val / Object.keys(playersStillPlaying).length);
+                            }
+                        }
+                    }
+                }
+                roundTreasure += card.val % Object.keys(playersStillPlaying).length;
+                io.emit("showBtns", {players: playersStillPlaying});
+            }else{
+                playerResponses = 0;
+                endRound();
+                return;
+            }
+            playerResponses = 0;
+            function endRound(){
+                deck.cards.concat(deck.inPlay);
+                deck.inPlay = [];
+                deck.shuffle();
+                round++;
+                if (round == 6){
+                    let winner = players[0];
+                    for(let i=1; i<players.length; i++){
+                        if(players[i].points > winner.points){
+                            winner = players[i];
+                        }
+                    }
+                    io.emit("hasWinner", winner);
+                    return;
+                }
+                roundTreasure = 0;
+                playersAboutToLeave = {};
+                playersAlreadyGone = {};
+                playersStillPlaying = {};
+                for(let p in players){
+                    playersStillPlaying[players[p].id] = true;
+                }
+                io.emit("newRound", round);
+            }
+        }
     }
 
     socket.on("userCreate", function(data){
         players.push(new Player(data.name, socket.id));
+        playersStillPlaying[socket.id] = true;
         // send all players/info to newb
         socket.emit("newPlayerSetup", {response: players});
         // send newb info to all players
@@ -111,23 +198,46 @@ io.sockets.on('connection', function (socket) {
           io.emit("startGame");
           deck.shuffle();
           var card = deck.drawCard();
-          io.emit("showCard", {card: card});
-
-          io.emit("showBtns", {players: players});
+          for(let p in playersStillPlaying){
+              for(let idx in players){
+                  if (players[idx].id == p){
+                      players[idx].currentTreasure += Math.floor(card.val / Object.keys(playersStillPlaying).length);
+                  }
+              }
+          }
+          roundTreasure += card.val % Object.keys(playersStillPlaying).length;
+          io.emit("showCard", {card: card, allPlayers: players});
+          io.emit("showBtns", {players: playersStillPlaying});
         }
     })
 
-    socket.on("addCard", function(data){
-        var card = deck.drawCard();
-        io.emit("showCard", {card: card});
-        console.log(card);
+    socket.on("startRound", function(){
+        playerResponses++;
+        if (playerResponses == players.length){
+            deck.shuffle();
+            var card = deck.drawCard();
+            for(let p in playersStillPlaying){
+                for(let idx in players){
+                    if (players[idx].id == p){
+                        players[idx].currentTreasure += Math.floor(card.val / Object.keys(playersStillPlaying).length);
+                    }
+                }
+            }
+            roundTreasure += card.val % Object.keys(playersStillPlaying).length;
+            io.emit("showCard", {card: card, allPlayers: players});
+            io.emit("showBtns", {players: playersStillPlaying});
+            playerResponses = 0;
+        }
     })
 
     socket.on("keepPlaying", function(){
-        playersInRound.push(socket.id);
+        playerResponses++;
         checkStatus();
     });
     socket.on("notPlaying", function(){
-        
+        delete playersStillPlaying[socket.id];
+        playersAboutToLeave[socket.id] = true;
+        playerResponses++;    
+        checkStatus();
     });
 })
--- static/js/js.js
+++ static/js/js.js
@@ -5,34 +5,32 @@ $(document).ready(function(){
 
     socket.emit("userCreate", {name: username});
     socket.on("newPlayerSetup", function(data) {
-      for (let p in data.response) {
-        let pBox = document.createElement('div');
-        pBox.className = "player";
-        if (p == data.response.length-1) {
-          pBox.id = data.response[0].id;
+        let identity = document.getElementById("players_wrap").setAttribute("socket", data.response[data.response.length-1].id);
+        for (let p in data.response) {
+            let pBox = document.createElement('div');
+            pBox.className = "player";
+            pBox.id = data.response[p].id;
+            pBox.innerHTML = "<h4>" + data.response[p].username + "</h4><p>Total Treasure: " + data.response[p].points + "</p><p>Current Treasure: " + data.response[p].currentTreasure + "</p>";
+            document.getElementById('players_wrap').appendChild(pBox);
         }
-        pBox.innerHTML = "<h4>" + data.response[p].username + "</h4><p>" + data.response[p].points + "</p>";
-        document.getElementById('players_wrap').appendChild(pBox);
-      }
     });
     socket.on("newPlayerAdded", function(data) {
         let pBox = document.createElement('div');
         pBox.className = "player";
-        pBox.innerHTML = "<h4>" + data.response.username + "</h4><p>" + data.response.points + "</p>";
+        pBox.id = data.response.id;
+        pBox.innerHTML = "<h4>" + data.response.username + "</h4><p>Total Treasure: " + data.response.points + "</p><p>Current Treasure: " + data.response.currentTreasure + "</p>";
         document.getElementById('players_wrap').appendChild(pBox);
 
     });
-
-    // $("#add_card").click(function(){
-    //     socket.emit("addCard");
-    // })
 
     socket.on("showCard", function(data){
         let pBox = document.createElement('img');
         pBox.className = "card";
         pBox.setAttribute("src", data.card.img);
         document.getElementById('cards_wrap').appendChild(pBox);
-        console.log(data.card);
+        for(let p in data.allPlayers){
+            document.getElementById(data.allPlayers[p].id).innerHTML = "<h4>" + data.allPlayers[p].username + "</h4><p>Total Treasure: " + data.allPlayers[p].points + "</p><p>Current Treasure: " + data.allPlayers[p].currentTreasure + "</p>"
+        }
     })
 
     socket.on("startGame", function() {
@@ -41,19 +39,42 @@ $(document).ready(function(){
 
     socket.on("showBtns", function(data){
         for(let p in data.players){
-            let play = document.createElement('button');
-            let leave = document.createElement('button');
-            play.textContent = "play";
-            leave.textContent = "leave";
-            play.setAttribute("class", "play");
-            leave.setAttribute("class", "leave");
-            document.getElementById(data.players[p].id).appendChild(play);
-            document.getElementById(data.players[p].id).appendChild(leave);
+            if(data.players[p]){
+                if(document.getElementById("players_wrap").getAttribute("socket") == p){
+                    let play = document.createElement('button');
+                    let leave = document.createElement('button');
+                    play.textContent = "play";
+                    leave.textContent = "leave";
+                    play.setAttribute("class", "play");
+                    leave.setAttribute("class", "leave");
+                    document.getElementById(p).appendChild(play);
+                    document.getElementById(p).appendChild(leave);
+                }
+            }else{
+                let msg = document.createElement('p');
+                msg.innerHTML = "Player has left this round.";
+                document.getElementById(p).appendChild(msg);
+            }
         }
     })
-    
+    socket.on("newRound", function(round){
+        $("#cards_wrap").html("");
+        $("#cards_wrap").html("<legend>Round " + round + "</legend>");
+        // alert("New Round Starting");
+        // socket.emit("startRound");
+    })
+    socket.on("hasWinner", function(winner){
+        alert(`${winner.username} has won!`);
+        location.reload();
+    })
 })
 $(document).on("click", ".play", function(){
     socket.emit("keepPlaying");
-    
+    $(".play").remove();
+    $(".leave").remove();
+})
+$(document).on("click", ".leave", function(){
+    socket.emit("notPlaying");
+    $(".play").remove();
+    $(".leave").remove();
 })
